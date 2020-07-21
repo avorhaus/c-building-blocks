1926cb70fcad60ffd6db53fe80b832ebea2b5109
av <vorhaus2214@gmail.com>
2020-07-21 10:09:18 -0500

    Completed accumulate

--- algorithms/accumulate/accumulate.c
+++ algorithms/accumulate/accumulate.c
@@ -2,13 +2,25 @@
 // Accumulate algotithm on integer arrays.
 
 #include "accumulate.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int accumulate(
-    int array[], 
-    size_t first, 
-    size_t last, 
-    int start, 
+    int array[],
+    size_t first,
+    size_t last,
+    int start,
     binary_op_f op)
 {
+  if(first>=last){
     return 0;
+  }
+  //printf("passed the valid indices test\n");
+  int accum = start;
+  for(int i = first; i < last; i++){
+    accum = op(accum,array[i]);
+    //printf("%d\n", accum);
+  }
+  return accum;
 }
